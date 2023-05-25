7e269d790bef1deb9fbe025916f2db7df4d4bf92
OXAYR <130404981+OXAYR@users.noreply.github.com>
2023-05-26 02:41:19 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './Style.css';
+import Information from './components/information';
+function App() {
+  return (
+    <div className="App">
+      <Information />
+    </div>
+  );
+}
+
+export default App;
